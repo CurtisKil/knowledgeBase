3e64bb236a88e8b6afe741d7cd84367531fed90a
lucidck <curtiskilgore@hotmail.com>
2020-06-07 21:04:14 -0700

    Created Views Files

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
-const express = require('express')
+const express = require("express");
+const path = require("path");
 
-const app = express()
+//  Init App
+const app = express();
 
-// Get request to homepage
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+// Load View Engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
+// Home Route
+app.get("/", function (req, res) {
+  let articles = [
+    {
+      id: 1,
+      title: "Article One",
+      author: "Curtis Kilgore",
+      body: "This is article one",
+    },
+    {
+      id: 2,
+      title: "Article Two",
+      author: "Joshy Jay",
+      body: "This is article two",
+    },
+    {
+      id: 3,
+      title: "Article Three",
+      author: "Curty Kay",
+      body: "This is article Three",
+    },
+  ];
+  res.render("index", {
+    title: "Articles",
+    articles: articles,
+  });
+});
+
+app.get("/articles/add", function (req, res) {
+  res.render("add_article", {
+    title: "Add Article",
+  });
+});
+
+// Start Server
 app.listen(3000, function () {
-    console.log('Server started on port 3000...');
-})
+  console.log("Server started on port 3000...");
+});
