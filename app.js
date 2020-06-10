d4ca25e48accf7ecde1425a5b18548783b83fddb
lucidck <curtiskilgore@hotmail.com>
2020-06-09 19:50:19 -0700

    Added Passport & User Registration

--- app.js
+++ app.js
@@ -102,7 +102,9 @@ app.get("/", function (req, res) {
 
 // Bring In Route Files
 let articles = require("./routes/articles");
+let users = require("./routes/users");
 app.use("/articles", articles);
+app.use("/users", users);
 
 // Start Server
 app.listen(3000, function () {
